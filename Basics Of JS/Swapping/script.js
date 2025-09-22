9bf86b1f3cfe63864a019b7bc20718fe12d6a69f
Balia077 <balaramd894@gmail.com>
2025-09-22 18:14:19 +0530

    Refactor swapping methods in script.js for clarity and organization

--- Basics Of JS/Swapping/script.js
+++ Basics Of JS/Swapping/script.js
@@ -1,9 +1,27 @@
 let a = 7;
 let  b = 18;
 
-let temp = a;    // 7
-a = b;          // 18
-b = temp;       // 7
+// Method 1 : Using third variable
+
+//let temp = a;    // 7
+//a = b;          // 18
+//b = temp;       // 7
+
+//console.log(a);  //output : 18
+//console.log(b);  //output : 7
+
+//Metthod 2 : using arithmetic operators
+
+//a = a + b;   // 25
+//b = a - b;   // 7
+//a = a - b;   // 18
+
+//console.log(a);  //output : 18
+//console.log(b);  //output : 7
+
+//Method 3 : using destructuring
+
+[a, b] = [b, a];
 
 console.log(a);  //output : 18
 console.log(b);  //output : 7
