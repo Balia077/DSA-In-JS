eb45ee2c4b807f3c895d410d94caac70202891f0
Balia077 <balaramd894@gmail.com>
2025-09-22 13:07:03 +0530

    Add initial HTML and JavaScript files for user age input examples

---
+++ Basics Of JS/Prompt in JS/script.js
@@ -0,0 +1,7 @@
+let age = prompt("Enter your age");
+console.log(age); // 19 as a string
+console.log(typeof age); // string
+
+let Age = Number(prompt("Enter your age"));
+console.log(Age); // 19 as a number
+console.log(typeof Age); // Number
