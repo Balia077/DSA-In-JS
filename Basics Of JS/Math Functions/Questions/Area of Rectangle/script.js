0e4ee5fd2af7475aa85e89c5845c710a8db55bbb
Balia077 <balaramd894@gmail.com>
2025-09-24 18:06:42 +0530

    Add HTML and JavaScript files for Area of Rectangle calculations

---
+++ Basics Of JS/Math Functions/Questions/Area of Rectangle/script.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+
+let area = a * b;
+let parameters = 2 * (a + b);
+
+console.log("Area of Rectangle is: " + area);
+console.log("Perimeter of Rectangle is: " + parameters);
