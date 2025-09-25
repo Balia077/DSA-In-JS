bda65eb1cb2d20aea42386f25c9e20bfec358acc
Balia077 <balaramd894@gmail.com>
2025-09-25 12:19:46 +0530

    Add initial HTML and JavaScript files for Ternary Operator example

---
+++ Basics Of JS/Conditionals/Ternary Operator/script.js
@@ -0,0 +1,10 @@
+// Ternary Operator
+// Syntax: condition ? expr1 : expr2
+
+console.log(112 > 13 ? "112 is greater" : "13 is greater");
+
+// Nested Ternary Operator
+
+let num = 0;
+console.log(num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero");
+// Output: Zero
